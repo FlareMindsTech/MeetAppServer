23b1d2ec181d9f39f729473f3710149e57e00306
karthikeyan0704 <karthikeyangnanasekar07@gmail.com>
2025-12-10 11:39:37 +0530

    API routes by splitting into separate files for better organization and maintainability

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+const router = express.Router();
+import auth from "../middleware/authMiddleware.js";
+import checkRoles from "../middleware/rolesMiddleware.js";
+
+// Controllers
+import { register, login, resetPassword, requestPasswordReset, logout } from "../controller/authController.js";
+
+// Roles
+const ownerOnly = checkRoles(["owner"]);
+const adminOnly = checkRoles(["owner", "admin"]);
+
+// --- PUBLIC AUTH ---
+router.post("/auth/register", register);
+router.post("/auth/login", login);
+router.post("/auth/forgot-password", requestPasswordReset);
+router.post("/auth/reset-password/:resetToken", resetPassword);
+
+// --- ADMIN AUTH ---
+router.post("/auth/owner/create-admin", auth, ownerOnly, register);
+router.post("/auth/admin/create-student", auth, adminOnly, register);
+router.post("/admin/login", login);
+router.post("/admin/logout", auth, adminOnly, logout);
+
+export default router;
